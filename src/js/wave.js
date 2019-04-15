4da7ebc65eaa118b95b6cb39fa96609af5a64832
Eric <1eric.hoffman@gmail.com>
2019-04-15 14:38:47 -0500

    audio analyzer works.  need sound to play on start.  mess with positioning as well

--- src/js/wave.js
+++ src/js/wave.js
@@ -1,62 +1,39 @@
-// Create the Audio Context
-
-var context = new AudioContext();
-var analyser = context.createAnalyser();
-var WIDTH = 600;
-var HEIGHT = 600;
-
-function playSound() {
-    var osc = context.createOscillator();
-    osc.frequency.value = 60;
-    osc.type = 'square';
-    
-    oscGain = context.createGain();
-    oscGain.gain.value = 0.2;
-
-    osc.start(context.currentTime);
-    osc.stop(context.currentTime + 3);
-
-    osc.connect(oscGain);   
-    oscGain.connect(analyser); // Connect oscillator to analyser node
-    analyser.connect(context.destination);
+// Create a new instance of an audio object and adjust some of its properties
+var audio = new Audio();
+audio.src = 'Eulogy.mp3';
+audio.controls = true;
+audio.loop = true;
+audio.autoplay = true;
+// Establish all variables that your Analyser will use
+var canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height;
+// Initialize the MP3 player after the page loads all of its HTML into the window
+window.addEventListener("load", initMp3Player, false);
+function initMp3Player(){
+  document.getElementById('audio_box').appendChild(audio);
+  context = new AudioContext(); // AudioContext object instance
+  analyser = context.createAnalyser(); // AnalyserNode method
+  canvas = document.getElementById('analyser_render');
+  ctx = canvas.getContext('2d');
+  // Re-route audio playback into the processing graph of the AudioContext
+  source = context.createMediaElementSource(audio); 
+  source.connect(analyser);
+  analyser.connect(context.destination);
+  frameLooper();
 }
-
-
-var canvas = document.querySelector('.visualizer');
-var myCanvas = canvas.getContext("2d");
-
-analyser.fftSize = 256;
-var bufferLength = analyser.frequencyBinCount;
-console.log(bufferLength);
-var dataArray = new Uint8Array(bufferLength);
-
-myCanvas.clearRect(0, 0, WIDTH, HEIGHT);
-
-function draw() {
-    drawVisual = requestAnimationFrame(draw);
-
-    analyser.getByteFrequencyData(dataArray);
-
-    myCanvas.fillStyle = 'rgb(0, 0, 0)';
-    myCanvas.fillRect(0, 0, WIDTH, HEIGHT);
-
-    var barWidth = (WIDTH / bufferLength) * 2.5;
-    var barHeight;
-    var x = 0;
-
-    for(var i = 0; i < bufferLength; i++) {
-      barHeight = dataArray[i];
-
-      myCanvas.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
-      myCanvas.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight/2);
-
-      x += barWidth + 1;
-    }
-};
-
-var analyserButton = document.getElementById("myAnalyserButton")
-
-analyserButton.addEventListener('click', function() {
-  playSound();
-  draw();
-});
+// frameLooper() animates any style of graphics you wish to the audio frequency
+// Looping at the default frame rate that the browser provides(approx. 60 FPS)
+function frameLooper(){
+  window.requestAnimationFrame(frameLooper);
+  fbc_array = new Uint8Array(analyser.frequencyBinCount);
+  analyser.getByteFrequencyData(fbc_array);
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+  ctx.fillStyle = '#00CCFF'; // Color of the bars
+  bars = 100;
+  for (var i = 0; i < bars; i++) {
+    bar_x = i * 3;
+    bar_width = 2;
+    bar_height = -(fbc_array[i] / 2);
+    //  fillRect( x, y, width, height ) // Explanation of the parameters below
+    ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
+  }
+}
